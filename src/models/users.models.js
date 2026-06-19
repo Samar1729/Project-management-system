a42fb4d657f3514dec17d0721af7a9422c3474bd
Samar <meet.suraj1994@gmail.com>
2026-06-19 15:30:21 +0530

    starting the authentication part

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -57,11 +57,38 @@ const userSchema = new mongoose.Schema(
         }
     },
     {
-        timestamps : true
+        timestamps: true
     }
 )
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next() 
+})
 
+/*
+How it is working in your code
+Let’s trace the execution:
+
+The Trigger: Somewhere in your controllers folder, you call await newUser.save().
+
+The Interception: Before Mongoose actually sends the data to MongoDB, it stops and checks if there are any pre("save") hooks registered. It finds yours.
+
+The Logic:
+
+It checks if(!this.isModified("password")). This is a clever optimization. If you are updating the user's email but not their password, you don't need to re-hash the password. It skips the heavy hashing work.
+
+If the password was modified, it runs bcrypt.hash(this.password, 10).
+
+It updates this.password with the secure, hashed version.
+
+The Release: Only after the hashing is finished, next() is called, and Mongoose proceeds to write the document to your MongoDB collection.
+
+*/
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password , this.password)
+}
 
 export const User = mongoose.model("User", userSchema)
-
